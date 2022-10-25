edee14af541d5cb3a8b4649b4e6a596d4ad9aa33
abdolazizsalimi <aziz.s6882@gmail.com>
2022-10-25 17:23:13 +0330

    adding swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('test example')
+    .setDescription('The test API description')
+    .setVersion('1.0')
+    .addTag('test')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
