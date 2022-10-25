a793be46f8a221ac67a1360d934b6dd497b29627
abdolazizsalimi <aziz.s6882@gmail.com>
2022-10-25 18:34:18 +0330

    add class validator config for userdto

--- src/users/dtos/CreateUser.dto.ts
+++ src/users/dtos/CreateUser.dto.ts
@@ -1,10 +1,36 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto{
+    @ApiProperty()
+    @IsString()
     @IsNotEmpty()
-    username : string;
-    @IsEmail()
-    @IsNotEmpty()
-    email : string;
+    username: string
 
+    @ApiProperty()
+    @IsString()
+    fullname: string
+
+    @ApiProperty()
+    @IsString()
+    @IsEmail()
+    email: string
+
+    @ApiProperty()
+    @IsString()
+    @MinLength(6)
+    confirmPassword: string
+
+    // @ApiProperty({ enum: Role })
+    // @IsEnum(Role)
+    // role: Role
+
+    @ApiProperty()
+    @IsString()
+    @MinLength(6)
+    password: string
+
+    @ApiProperty()
+    @IsString()
+    phoneNumber: string
 }
